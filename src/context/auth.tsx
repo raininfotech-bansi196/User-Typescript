1df4e7d30c6b7db79f0901a8d53433bde86ba8c8
raininfotech-bansi196 <bansi196@webmail.rainit.in>
2025-05-06 17:01:13 +0530

    update

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -4,6 +4,7 @@ import { fetchApi } from '@/utils/frontend';
 import { getCookie } from 'cookies-next';
 import { usePathname, useRouter } from 'next/navigation';
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 interface UserDetails {
     email: string;
@@ -30,6 +31,8 @@ interface AuthContextType {
     setUserDetails: (userDetails: UserDetails) => void;
     isConnectWallet: boolean;
     setIsConnectWallet: (isConnectWallet: boolean) => void;
+    isMaintenance: boolean;
+    setIsMaintenance: (isConnectWallet: boolean) => void;
 }
 
 const AuthContext = createContext<AuthContextType>({
@@ -41,25 +44,44 @@ const AuthContext = createContext<AuthContextType>({
     setUserDetails: () => { },
     isConnectWallet: false,
     setIsConnectWallet: () => { },
+    isMaintenance: false,
+    setIsMaintenance: () => { },
 });
 
 export const AuthContextProvider = (props: { children: React.ReactNode }) => {
     const [authTkn, setAuthTkn] = useState<string>('init');
     const [pageLoader, setPageLoader] = useState<boolean>(true);
     const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
-    const [isConnectWallet, setIsConnectWallet] = useState<boolean>(false)
+    const [isConnectWallet, setIsConnectWallet] = useState<boolean>(false);
+    const [isMaintenance, setIsMaintenance] = useState<boolean>(false);
     const router = useRouter();
-
     useEffect(() => {
         const token = getCookie("acsmailtkn");
         if (token) {
             setAuthTkn(token.toString());
-            getUserDetails();
+            getMaintennace();
         } else {
             router.push("/login");
         }
     }, []);
-
+    const getMaintennace = async () => {
+        try {
+            const response = await fetchApi(`/get-maintenance`, "", "GET");
+            if (response?.statusCode === 200) {
+                setIsMaintenance(response?.data?.maintananceStatus === 1 ? true : false);
+                getUserDetails();
+            } else {
+                if (response?.data?.message === "Unauthorized") {
+                    setAuthTkn(response?.data?.message)
+                }
+                toast.error(response?.data?.message)
+            }
+            setPageLoader(false);
+            console.log({ response });
+        } catch (error) {
+            console.log({ error });
+        }
+    }
     const getUserDetails = async () => {
         try {
             const res = await fetchApi(`/user-details`, "", "GET");
@@ -80,7 +102,7 @@ export const AuthContextProvider = (props: { children: React.ReactNode }) => {
     }, [path])
 
     return (
-        <AuthContext.Provider value={{ authTkn, setAuthTkn, pageLoader, setPageLoader, userDetails, setUserDetails, isConnectWallet, setIsConnectWallet }}>
+        <AuthContext.Provider value={{ authTkn, setAuthTkn, pageLoader, setPageLoader, userDetails, setUserDetails, isConnectWallet, setIsConnectWallet, isMaintenance, setIsMaintenance }}>
             {props.children}
         </AuthContext.Provider>
     );
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,14 @@ import { NextResponse } from "next/server";
 
 export async function middleware(request: any) {
     const token = request.cookies.get("acsmailtkn")?.value;
+    console.log(process.env.BASEURL, "process.env.BASEURL");
+    let response = await fetch(process.env.BASEURL + "maintenance-status.json")
+    let main = await response.json();
+    console.log({main});
+    
+    if (!['/maintenance'].includes(request.nextUrl.pathname) && main.status == 1) {
+        return NextResponse.redirect(new URL('/maintenance', request.url))
+    }
     if (!token && !['/login', '/email-verification', '/register', "/forgot-password", "/reset-password", '/'].includes(request.nextUrl.pathname)) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
